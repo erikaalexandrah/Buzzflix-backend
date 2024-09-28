a9e3214c57768b911dd618b03996672ac88d11ca
Erika Hernández <113314381+erikaalexandrah@users.noreply.github.com>
2024-09-28 17:34:18 -0400

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,11 @@ async function bootstrap() {
   
   // Habilitar CORS
   app.enableCors({
-    origin: '*',
+    origin: 'https://unimet-buzzflix.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, 
