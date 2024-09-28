b14bbb0b2a94208734ef94dda4aea602a2dceba0
Erika Hernández <113314381+erikaalexandrah@users.noreply.github.com>
2024-09-28 17:21:33 -0400

    fix: CORS problem frontend

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
   
   // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, 
+    credentials: true,
   });
+  
 
   app.useGlobalPipes(
     new ValidationPipe({
